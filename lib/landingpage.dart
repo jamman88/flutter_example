b28a44291c3a8162a18b321f5baa4cb986d2e8c9
jamman88 <jamman88@naver.com>
2021-07-14 13:02:32 +0900

    전근영 1번

---
+++ lib/landingpage.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Image.asset('assets/image/add.jpg',fit: BoxFit.cover,),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_example/test/next.dart';
+import 'landingpage.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -10,76 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      home: FirstPage(),
+      home: LandingPage(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        title: Text("안녕하세요 저는",style: TextStyle(color: Colors.red),),
-        centerTitle: true,
-      ),
-      body: Row(
-        children: [
-          GestureDetector(
-            onTap: (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder:(context) => NextPage(name: "마이클",isKorean: true,gender: "여자",age: 20,)),
-              );
-            },
-            child: Container(
-              child: Text("여기를 누르세요"),
-              color: Colors.pink,
-              height: 50,
-            ),
-          ),
-          Container(
-            width: 400,
-            height: 400,
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.yellow,
-                  )
-                ),
-                Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.cyanAccent,
-                    )
-                ),
-                Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.grey,
-                    )
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 400,
-            height: 400,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-          )
-        ],
-      ),
-    );
-  }
-  
-
-}
